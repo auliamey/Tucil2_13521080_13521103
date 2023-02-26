8c3ead6a8f9a500e93e31419bf2fc37bbf9e1424
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-02-26 15:38:10 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,55 +3,60 @@ from bruteforce import *
 from visualize import *
 from utility import *
 import time
+def main():
+    #I.S : user memasukkan jumlah titik dan dimensi titik
+    #F.S : menampilkan jarak terdekat antara 2 titik, titik pertama, titik kedua, jumlah perhitungan, dan waktu eksekusi
+    #Proses : main program
+    n = int(input("Masukkan jumlah titik : "))
+    dimensi = int(input("Masukkan dimensi titik : "))
+    if n > 0 and dimensi > 0:
+        array = randomPoint(n,dimensi)
+        sortbyx(array,0,n-1)
+        if dimensi == 3:
+            count = 0
+            t1 = time.time()
+            min,minPoint1,minPoint2,count =closestPairBf(array,n,dimensi,count)
+            t2 = time.time()
+            print("Jarak terdekat antara 2 titik adalah : ",min)
+            print("Titik pertama : ",minPoint1)
+            print("Titik kedua : ",minPoint2)
+            print("Jumlah perhitungan : ",count)
+            print("Waktu eksekusi : ",t2-t1)
 
-n = int(input("Masukkan jumlah titik : "))
-dimensi = int(input("Masukkan dimensi titik : "))
-if n > 0 and dimensi > 0:
-    array = randomPoint(n,dimensi)
-    sortbyx(array,0,n-1)
-    if dimensi == 3:
-        count = 0
-        t1 = time.time()
-        min,minPoint1,minPoint2,count =closestPairBf(array,n,dimensi,count)
-        t2 = time.time()
-        print("Jarak terdekat antara 2 titik adalah : ",min)
-        print("Titik pertama : ",minPoint1)
-        print("Titik kedua : ",minPoint2)
-        print("Jumlah perhitungan : ",count)
-        print("Waktu eksekusi : ",t2-t1)
+            count = 0
+            t1 = time.time()
+            min,minPoint1,minPoint2,count = closestPairDnC(array,n,dimensi,count)
+            t2 = time.time()
+            print("Jarak terdekat antara 2 titik adalah : ",min)
+            print("Titik pertama : ",minPoint1)
+            print("Titik kedua : ",minPoint2)
+            print("Jumlah perhitungan : ",count)
+            print("Waktu eksekusi : ",t2-t1)
 
-        count = 0
-        t1 = time.time()
-        min,minPoint1,minPoint2,count = closestPairDnC(array,n,dimensi,count)
-        t2 = time.time()
-        print("Jarak terdekat antara 2 titik adalah : ",min)
-        print("Titik pertama : ",minPoint1)
-        print("Titik kedua : ",minPoint2)
-        print("Jumlah perhitungan : ",count)
-        print("Waktu eksekusi : ",t2-t1)
+            visualize(array,minPoint1,minPoint2)
+        elif dimensi>0 and dimensi != 3:
+            count = 0
+            t1 = time.time()
+            min,minPoint1,minPoint2,count =closestPairBf(array,n,dimensi,count)
+            t2 = time.time()
+            print("Jarak terdekat antara 2 titik adalah : ",min)
+            print("Titik pertama : ",minPoint1)
+            print("Titik kedua : ",minPoint2)
+            print("Jumlah perhitungan : ",count)
+            print("Waktu eksekusi : ",t2-t1)
 
-        visualize(array,minPoint1,minPoint2)
-    elif dimensi>0 and dimensi != 3:
-        count = 0
-        t1 = time.time()
-        min,minPoint1,minPoint2,count =closestPairBf(array,n,dimensi,count)
-        t2 = time.time()
-        print("Jarak terdekat antara 2 titik adalah : ",min)
-        print("Titik pertama : ",minPoint1)
-        print("Titik kedua : ",minPoint2)
-        print("Jumlah perhitungan : ",count)
-        print("Waktu eksekusi : ",t2-t1)
-
-        count = 0
-        t1 = time.time()
-        min,minPoint1,minPoint2,count = closestPairDnC(array,n,dimensi,count)
-        t2 = time.time()
-        print("Jarak terdekat antara 2 titik adalah : ",min)
-        print("Titik pertama : ",minPoint1)
-        print("Titik kedua : ",minPoint2)
-        print("Jumlah perhitungan : ",count)
-        print("Waktu eksekusi : ",t2-t1)
-    else: 
-        print("Dimensi tidak valid")
-else:
-    print("Jumlah titik atau dimensi tidak valid")
+            count = 0
+            t1 = time.time()
+            min,minPoint1,minPoint2,count = closestPairDnC(array,n,dimensi,count)
+            t2 = time.time()
+            print("Jarak terdekat antara 2 titik adalah : ",min)
+            print("Titik pertama : ",minPoint1)
+            print("Titik kedua : ",minPoint2)
+            print("Jumlah perhitungan : ",count)
+            print("Waktu eksekusi : ",t2-t1)
+        else: 
+            print("Dimensi tidak valid")
+    else:
+        print("Jumlah titik atau dimensi tidak valid")
+    
+main()
